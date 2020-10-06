5c2c589e5a8c35232404081130cd1a15e4c4c064
Dalgona <me@dalgona.dev>
2020-10-06 10:24:32 +0900

    feat: cherry pick bitmap glyphs from neodgm and rename them

---
+++ lib/neodgm_pro/bitmap_font.ex
@@ -0,0 +1,44 @@
+defmodule NeoDGMPro.BitmapFont do
+  @spec get_sources() :: [module()]
+  def get_sources do
+    [
+      base_glyph_sources(),
+      pro_glyph_sources()
+    ]
+    |> List.flatten()
+  end
+
+  defp base_glyph_sources do
+    [
+      BlockElements,
+      BoxDrawing,
+      BraillePatterns,
+      Dingbats,
+      EnclosedAlphanumerics,
+      EnclosedCJKLetters,
+      GeometricShapes,
+      HalfwidthAndFullwidthForms,
+      HangulCompatibilityJamo,
+      HangulJamo,
+      HangulJamoSource,
+      HangulSyllables,
+      NumberForms,
+      Trigrams,
+      Components.Diacritics,
+      Components.Enclosed,
+      Components.Fractions
+    ]
+    |> Enum.map(&Module.concat(NeoDGM.BitmapFont, &1))
+  end
+
+  defp pro_glyph_sources do
+    [
+      BasicLatin,
+      GeneralPunctuation,
+      Latin1Supplement,
+      AlphabeticPresentationForms,
+      Components.ShortCapitals
+    ]
+    |> Enum.map(&Module.concat(__MODULE__, &1))
+  end
+end

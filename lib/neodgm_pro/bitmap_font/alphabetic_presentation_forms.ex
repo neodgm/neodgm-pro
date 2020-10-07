0612d3b4b21d16be977aba934f6bc13310933a3c
Dalgona <me@dalgona.dev>
2020-10-07 12:35:51 +0900

    feat: modify glyphs for ff, fi, fl, ffi, ffl ligatures
    Give them some more spaces between ligature components.

--- lib/neodgm_pro/bitmap_font/alphabetic_presentation_forms.ex
+++ lib/neodgm_pro/bitmap_font/alphabetic_presentation_forms.ex
@@ -3,46 +3,64 @@ use PixelFont.GlyphSource
 glyph_source NeoDGMPro.BitmapFont.AlphabeticPresentationForms do
   composite_glyph unicode: 0xFB00 do
     component {:unicode, ?f}, 0, 0
-    component {:unicode, ?f}, 4, 0
+    component {:unicode, ?f}, 5, 0
   end
 
   composite_glyph unicode: 0xFB01 do
     component {:name, "f.liga"}, 0, 0
-    component {:unicode, 0x0131}, 4, 0
+    component {:unicode, 0x0131}, 5, 0
   end
 
   composite_glyph unicode: 0xFB02 do
     component {:unicode, ?f}, 0, 0
-    component {:unicode, ?l}, 4, 0
+    component {:name, "l.liga"}, 5, 0
   end
 
   composite_glyph unicode: 0xFB03 do
     component {:unicode, ?f}, 0, 0
-    component {:name, "f.liga"}, 4, 0
-    component {:unicode, 0x0131}, 8, 0
+    component {:name, "f.liga"}, 5, 0
+    component {:unicode, 0x0131}, 10, 0
   end
 
   composite_glyph unicode: 0xFB04 do
     component {:unicode, ?f}, 0, 0
-    component {:unicode, ?f}, 4, 0
-    component {:unicode, ?l}, 8, 0
+    component {:unicode, ?f}, 5, 0
+    component {:name, "l.liga"}, 10, 0
   end
 
   bmp_glyph name: "f.liga" do
-    advance 7
-    bounds 0..7, 0..10
+    advance 8
+    bounds 0..8, 0..10
 
     data """
-    0011110
-    0110011
-    0110000
-    1111100
-    0110000
-    0110000
-    0110000
-    0110000
-    0110000
-    0110000
+    00111110
+    01100011
+    01100000
+    11111000
+    01100000
+    01100000
+    01100000
+    01100000
+    01100000
+    01100000
+    """
+  end
+
+  bmp_glyph name: "l.liga" do
+    advance 5
+    bounds 0..4, 0..10
+
+    data """
+    1100
+    0110
+    0110
+    0110
+    0110
+    0110
+    0110
+    0110
+    0110
+    0011
     """
   end
 end

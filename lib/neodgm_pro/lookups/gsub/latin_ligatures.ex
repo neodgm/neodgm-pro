79667ffb4c73c90c5b570daee1f6395004c05841
Dalgona <me@dalgona.dev>
2021-12-18 22:57:55 +0900

    refactor: rewrite latin ligatures using DSL

--- lib/neodgm_pro/lookups/gsub/latin_ligatures.ex
+++ lib/neodgm_pro/lookups/gsub/latin_ligatures.ex
@@ -1,50 +1,22 @@
 use PixelFont.OTFLayout
 
 lookups NeoDGMPro.Lookups.GSUB.LatinLigatures, for: "GSUB" do
-  module do
-    alias PixelFont.TableSource.OTFLayout.Lookup
-    alias PixelFont.TableSource.GSUB
-    alias PixelFont.TableSource.GSUB.Ligature1
+  lookup :ligature, "Ligature substitutions ffi fi" do
+    feature "liga", %{"latn" => [:default]}
+
+    substitutions do
+      substitute 'ffi', 0xFB03
+      substitute 'fi', 0xFB01
+    end
   end
 
-  %Lookup{
-    owner: GSUB,
-    name: "Ligature substitutions ffi fi",
-    features: %{
-      "liga" => %{
-        # In Turkish language, the "dotless i" letter is used.
-        # Therefore ligatures for "ffi" and "fi" must be disabled.
-        "latn" => [:default]
-      }
-    },
-    type: 4,
-    subtables: [
-      %Ligature1{
-        substitutions: [
-          {'ffi', 0xFB03},
-          {'fi', 0xFB01}
-        ]
-      }
-    ]
-  }
+  lookup :ligature, "Ligature substitutions ffl fl ff" do
+    feature "liga", %{"latn" => [:default, "TRK "]}
 
-  %Lookup{
-    owner: GSUB,
-    name: "Ligature substitutions ffl fl ff",
-    features: %{
-      "liga" => %{
-        "latn" => [:default, "TRK "]
-      }
-    },
-    type: 4,
-    subtables: [
-      %Ligature1{
-        substitutions: [
-          {'ffl', 0xFB04},
-          {'fl', 0xFB02},
-          {'ff', 0xFB00}
-        ]
-      }
-    ]
-  }
+    substitutions do
+      substitute 'ffl', 0xFB04
+      substitute 'fl', 0xFB02
+      substitute 'ff', 0xFB00
+    end
+  end
 end

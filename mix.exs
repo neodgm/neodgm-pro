42768e307ef5ee4353456a884fb5a478735b0604
Dalgona <me@dalgona.dev>
2022-01-30 21:51:37 +0900

    build: update NeoDGM to ac65163

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NeoDGMPro.MixProject do
     [
       app: :neodgm_pro,
       version: "1.0.11",
-      elixir: "~> 1.10",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -19,7 +19,7 @@ defmodule NeoDGMPro.MixProject do
 
   defp deps do
     [
-      {:neodgm, github: "Dalgona/neodgm", ref: "10c0e8e1c214541fb766f83b261fb734d249d8e4"}
+      {:neodgm, github: "Dalgona/neodgm", ref: "ac6516358df2bc55c4d4004620784d93a24988b3"}
     ]
   end
 end

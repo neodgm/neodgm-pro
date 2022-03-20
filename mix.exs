b9c49330fb8c97c65cfb6662b8d0084abeccefb5
Dalgona <me@dalgona.dev>
2022-03-20 17:21:37 +0900

    build: update pixel_font to c86174f

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule NeoDGMPro.MixProject do
 
   defp deps do
     [
-      {:neodgm, github: "Dalgona/neodgm", ref: "ac6516358df2bc55c4d4004620784d93a24988b3"}
+      {:neodgm, github: "neodgm/neodgm", ref: "ac6516358df2bc55c4d4004620784d93a24988b3"},
+      {:pixel_font, github: "Dalgona/pixel_font", ref: "c86174f32ca5f4ab91e18872ad7f697579227ae4", override: true}
     ]
   end
 end

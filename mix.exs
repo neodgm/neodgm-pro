9fcdf6395b4cfa0f94c70476d667f8e75c0dd89e
Dalgona <me@dalgona.dev>
2022-05-25 18:39:52 +0900

    build: NeoDGM Pro v1.020

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NeoDGMPro.MixProject do
   def project do
     [
       app: :neodgm_pro,
-      version: "1.0.11",
+      version: "1.0.20",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
